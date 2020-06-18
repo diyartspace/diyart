4872878da7b532c565b50ed1c7b9f1aba699d5f4
Chau Nguyen <nmchau87@gmail.com>
2020-06-19 06:58:58 +0900

    feat: resize and show upload image (#2)

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,22 +1,58 @@
 import { FileUploader } from 'baseui/file-uploader'
 import { NextPage } from 'next'
-import { useCallback } from 'react'
+import Pica from 'pica/dist/pica'
+import { useCallback, useState } from 'react'
+import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch'
 
 import { PageHead } from '../components/common'
 
+const pica = new Pica()
+
 const HomePage: NextPage = () => {
+  const [image, setImage] = useState<string>(undefined)
+
   const onDropAccepted = useCallback((accepted: File[]) => {
     if (!accepted.length) {
       return
     }
     const imageFile: File = accepted[0]
-    console.log(imageFile)
+    const imageObject = new Image()
+    const fileLoader = new FileReader()
+    fileLoader.readAsDataURL(imageFile)
+    fileLoader.onload = function () {
+      if (typeof this.result === 'string') {
+        imageObject.src = this.result
+      }
+    }
+    imageObject.onload = async function () {
+      if (Math.max(imageObject.width, imageObject.height) > 1000) {
+        const offScreenCanvas = document.createElement('canvas')
+        if (imageObject.width < imageObject.height) {
+          offScreenCanvas.height = (imageObject.height * 1000) / imageObject.width
+          offScreenCanvas.width = 1000
+        } else {
+          offScreenCanvas.height = 1000
+          offScreenCanvas.width = (imageObject.width * 1000) / imageObject.height
+        }
+        await pica.resize(imageObject, offScreenCanvas)
+        setImage(offScreenCanvas.toDataURL())
+      } else {
+        setImage(imageObject.src)
+      }
+    }
   }, [])
 
   return (
     <>
       <PageHead title='Home' />
       <FileUploader multiple={false} onDropAccepted={onDropAccepted} />
+      {image && (
+        <TransformWrapper>
+          <TransformComponent>
+            <img src={image} />
+          </TransformComponent>
+        </TransformWrapper>
+      )}
     </>
   )
 }
